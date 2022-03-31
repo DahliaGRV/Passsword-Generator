6ae9913de780b3d0b164d55053fb22012bd52360
Dahlia Graves <dahlialolagraves@gmail.com>
2022-03-30 17:02:59 -0700

    more changes

--- script.js
+++ script.js
@@ -1,60 +1,53 @@
 
 function generatePassword() {
-  document.getElementById("generate").onclick = function () { myButton() };
-function myButton() {
-  var passwordLength = prompt("Please chose the character length for your password (between 8 and 128)")
-  console.log(passwordLength)
-}
+  
+    var passwordLength = prompt("Please chose the character length for your password (between 8 and 128)") 
 
-function makePassword() {
-  var lowercase = confirm("Include lowercase letters?");
+    var lowercase = confirm("Include lowercase letters?");
 
-  var uppercase = confirm("Include uppercase letters?");
+    var uppercase = confirm("Include uppercase letters?");
 
-  var numbers = confirm("Include numbers?");
+    var numbers = confirm("Include numbers?");
 
-  var special = confirm("Include special characters?");
-}
+    var special = confirm("Include special characters?");
 
-if (lowercase == true) {
-  function generateLower() {
-    const lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz"
-    return lowercaseAlphabet[Math.floor(Math.random() * lowercaseAlphabet.length)]
-  };
-  console.log(generateLower);
-} else if (uppercase === true) {
-  function generateUpper() {
-    const uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    return uppercaseAlphabet[Math.floor(Math.random() * uppercaseAlphabet.length)]
-  };
-  console.log(generateUpper);
-} else if (numbers === true) {
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * max - min)
-  };
-  console.log(getRandomInt)
-} else if (special === true) {
-  function generateSpecial() {
-    const specialCharacters = "!@#$%^&*()"
-    return generateSpecial[Math.floor(Math.random() * specialCharacter.length)]
+    if (lowercase === true) {
+      function generateLower() {
+        const lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz"
+        return lowercaseAlphabet[Math.floor(Math.random() * lowercaseAlphabet.length)]
+      };
+      console.log(generateLower);
+    } else if (uppercase === true) {
+      function generateUpper() {
+        const uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        return uppercaseAlphabet[Math.floor(Math.random() * uppercaseAlphabet.length)]
+      };
+      console.log(generateUpper);
+    } else if (numbers === true) {
+      function getRandomInt(min, max) {
+        return Math.floor(Math.random() * max - min)
+      };
+      console.log(getRandomInt)
+    } else if (special === true) {
+      function generateSpecial() {
+        const specialCharacters = "!@#$%^&*()"
+        return generateSpecial[Math.floor(Math.random() * specialCharacter.length)]
+      }
+    }
+    //TODO: your code goes here
+    return "password"
   }
-}
-  //TODO: your code goes here
-  return "password"
-}
 
 
-// Assignment Code, DO NOT EDIT ANTHING  BELOW THIS LINE
-var generateBtn = document.querySelector("#generate");
+  // Assignment Code, DO NOT EDIT ANTHING  BELOW THIS LINE
+  var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
-
+    passwordText.value = password;
+  }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
