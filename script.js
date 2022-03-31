cfc1adc74f3693b39d3e9bc14d9e240da21f1a8f
Dahlia Graves <dahlialolagraves@gmail.com>
2022-03-31 12:09:27 -0700

    up to date

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
 function generatePassword() {
 
-    var passwordLength = prompt("Please choose the character length for your password (between 8 and 128)") 
+  var passwordLength = prompt("Please choose the character length for your password (between 8 and 128)") 
 
     var lowercase = confirm("Include lowercase letters?");
 
@@ -12,31 +11,37 @@ function generatePassword() {
     var special = confirm("Include special characters?");
 
     // if (passwordLength)
-    
+  for (let i = 0; parseInt < passwordLength.length; i++) {
+    // const element = passwordLength[i];
+  }
     if (lowercase === true) {
       function generateLower() {
-        const lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz"
-        return lowercaseAlphabet[Math.floor(Math.random() * lowercaseAlphabet.length)]
+        const lowercaseAlphabet = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+        return lowercaseAlphabet[Math.floor(Math.random() * passwordLength.length)]
       };
       console.log(generateLower);
+
     } else if (uppercase === true) {
       function generateUpper() {
-        const uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        const uppercaseAlphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
         return uppercaseAlphabet[Math.floor(Math.random() * uppercaseAlphabet.length)]
       };
-      console.log(generateUpper);
     } else if (numbers === true) {
-      function getRandomInt(min, max) {
-        return Math.floor(Math.random() * max - min)
+      function gnerateNumber() {
+        const number = ["1","2","3","4","5","6","7","8","9","0"]
+        return generateNumber [Math.floor(Math.random() * number.length )]
       };
-      console.log(getRandomInt)
+
     } else if (special === true) {
       function generateSpecial() {
-        const specialCharacters = "!@#$%^&*()"
+        const specialCharacters = ["!","@","#","$","%","^","&","*","(",")"]
         return generateSpecial[Math.floor(Math.random() * specialCharacter.length)]
-      }
-    }
+      } 
+    } 
+  
+       // for(var i=0, i<=passwordLength.length);i++)
     //TODO: your code goes here
+    const allCharacters = lowercaseAlphabet.concat(uppercaseAlphabet, number,specialCharacter)
     return "password"
   }
 
